1fb5223c1deed9825adee4526dad79730d3dd328
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-11-05 16:34:56 +0100

    Add moves / vorgehens beispiele

---
+++ test/arrays.js
@@ -0,0 +1,32 @@
+(() => {
+
+    const testReportTitle = "Arrays and useful tips";
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  useful tips js tips - have a look at "mdn js XXX"
+// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+    let ok = [];
+
+    let personen = ["bob", "alice", "foo"];
+
+
+    let found = personen.find( element => {
+        console.log(element);                             // iterate over all, change elements
+        return element === "alice"                        // stop iteration
+    });
+
+    ok.push("alice" === found);
+
+    let array1 = [1, 18, 42, 16, 49];
+    let array2 = [1, 18, 42, 17, 49];
+
+    ok.push(!array1.every( (it, index) => it > 15));
+    // evaluate each item
+
+    ok.push(array1.some( (it, index) => it === array2[index]));
+    // gibt es iergend ein element, das in array 1 nicht in array 2 ist
+
+    report(testReportTitle, ok);
+})();
---
+++ test/moves.js
@@ -0,0 +1,46 @@
+(() => {
+
+    const testReportTitle = "Moves ";
+
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // Programming ist ein Vorgang
+    // Welche Natur hat der Vorgang?
+    // Aus welchen Teilen besteht er?
+    //
+    // Schrittfolgen (moves) muss man lernen.
+    // Dann kann man sie kombinieren und
+    // den Umständen gemäss anpassen.
+    //
+    // Bewusst werden, was man tut.
+    // Wir programmieren gemeinsam,
+    // Sie halten nach moves Ausschau.
+
+    // 0 Exploration:           technische Machbarkeit, nur lernen, Timebox
+    // 1 Start at the End:      statische Version des Resultats bauen, vor jeder dynamischen
+    // 2 Extract:               statische Werte durch Variablen ersetzen,
+    //                          Wiederholungen durch mappings/loops ersetzen.
+    // 3 Abstract:              Abstrahieren der Lösung, bennenn und in Konzepte fassen.
+    //                          Wenn man keinen guten Begriff für die Abstrahierung findet,
+    //                          sollte man den Schritt abbrechen.
+    // 4 Reorganize:            Teile so umorganisieren, dass man leichter mit ihnen arbeiten kann.
+    // 5 Release:               Die Lösung selbständig machen - ohne  geheimes Wissen oder Hilfestellung
+    //                          Vor jedem push auf shared Repo!
+    // 6 Retrospektive:         Was beibehalten, was nächstes mal anders machen?
+
+    let ok = [];
+
+    const collect = (10).times( n => n+1 );
+
+    ok.push(collect.length === 10);
+    ok.push(collect[0] === 1);
+    ok.push(collect[9] === 11);
+
+    console.error("fehler");  // erster Schritt ist es technisch möglich den Fehler sichtbar zu machen...
+
+
+
+
+
+    report(testReportTitle, ok);
+})();
