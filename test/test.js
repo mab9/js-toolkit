14bd1b80ae71f2af53f82587e121b586266ef9c0
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-11-06 22:59:54 +0100

    Add some fancy js function tricks

--- test/test.js
+++ test/test.js
@@ -89,8 +89,82 @@
 
     ok.push(me.getName() === "Sherlok");
 
-// xs1_.eq(ys1_) && ! xs2_.eq(ys2_) && xs3_.eq(ys3_) && ! xs3_.eq(ys4_) && ! xs4_.eq(ys3_)
 
-    const eq =
+    ok.push(true);
+    // ok.push(false);
+    /*
+        const failSafe = x => y => z => {
+            if (y(z)) {
+                ;
+            } else {
+                return x;
+            }
+        };
+
+        const doError = x => {
+            throw new Error()
+        };
+        const errorCount = failSafe(1)(doError);
+
+        ok.push(failSafe(false)(x => x)(true) && failSafe(true)(doError)(null) && errorCount(null) === 1); */
+
+
+    //ok.push(false);
+
+
+    // 14a array equivalence
+    Array.prototype.eq = function (second) {
+        const first = this;
+        if (first.length !== second.length) {
+            return false;
+        }
+        return !first.some((it, index) => second[index] !== it);
+    };
+
+    ok.push(![1, 15, 39].eq([1, 15, 39, 4]));
+    ok.push([1, 15, 39, 4].eq([1, 15, 39, 4]));
+    ok.push(![1, 15, 39, 4].eq([1, 15, 39, 5]));
+
+    // 15 es 6 classes
+    // 16 prototypes and inhertiance
+    // 16a function composition
+
+    // 1. add protoype then
+    // 2. execute function
+    // 3. return passed function
+
+    /*
+       A key to function composition is having functions that are composable.
+       A composable function should have 1 input argument and 1 output value.
+
+       https://medium.com/javascript-scene/curry-and-function-composition-2c208d774983
+     */
+    const g = n => n + 1;
+    const f = n => n * 2;
+    const h = x => f(g(x));
+    ok.push(h(20) === 42);
+
+    // other way -> composes only 2 functions at a time
+    const compose = (f, g) => x => f(g(x));
+    ok.push(compose(f,g)(20) === 42);
+
+    // way to compose as many functions as the memory is funny
+    const compose2 = (...fns) => x => fns.reduceRight((y, f) => f(y), x);
+    ok.push(compose2(f,f,g,g)(20) === 88); // 1. g, 2. g, 3. f, 4. f
+
+    Function.prototype.then = (fnc) => {
+        console.info("then was invoked");
+        console.info(this);
+        return fnc;
+    };
+
+    let mabs = x => console.info("this is mabs: " + x);
+
+    //const h = x => f(g(x));
+    mabs.then(f)(20);
+
+
+    // 17 object encoding without classes, this, or new
+    // 18a failsafe callback
     report(testReportTitle, ok);
 })();
--- todo/todo.js
+++ todo/todo.js
@@ -1,8 +1,20 @@
+let container = undefined;
+const row = `
+        <tr>
+            <td><input type="text" placeholder="Add todo..."></td>
+            <td><input type="checkbox"></td>
+        </tr>`;
 
-function startTodo() {
 
+
+function startTodo(startContainer) {
+    container = startContainer;
 }
 
-function addTodo() {
-    
+function addTodo(openTasks) {
+    container.innerHTML += row;
+    //let openTasks = document.getElementById('numberOfTasks');
+    openTasks.innerText = Number(openTasks.innerText) + 1;
 }
+
+
