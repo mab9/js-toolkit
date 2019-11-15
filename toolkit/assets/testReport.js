dbf7c2344c9814384df413d7b08d1edd39ac4197
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-11-15 22:11:52 +0100

    Change display of failed tests

--- toolkit/assets/testReport.js
+++ toolkit/assets/testReport.js
@@ -1,18 +1,29 @@
 ////////////////////////////////////////////////////////////////////////////
 // Test Report - print test results
 
-function report(module, ok) {
+// #E8F3E8
+// #92C591
+// #559E54
+// #305A30
+// #0A3409
 
-    if (ok.every(elem => elem)) {
-        document.writeln(`
-            <div class="w3-panel w3-card">
-              <p> ${ok.length} passed - module: ${module}</p>
-            </div>`);
-    } else {
-        let failed = ok.filter(x => x === false);
-        document.writeln(`
-            <div class="w3-panel w3-card">
-              <p> ${failed.length} failed - module: ${module}</p>
-            </div>`);
+function report(module, ok) {
+    let passed = ok.filter(x => x === true).length;
+    let marked = "";
+    let button = "";
+    if (passed < ok.length) {
+        marked = "w3-flat-sun-flower";
+        button = `<a href="" class="w3-button w3-right w3-flat-midnight-blue">Show</a>`;
     }
+
+    //ok.filter(x => x === false).every(console.error);
+
+
+
+    document.writeln(`
+       <div class="w3-panel w3-card ${marked}">
+          <p> ${passed} / ${ok.length} passed - ${module} ${button}</p>
+       </div>
+    `);
+
 }
