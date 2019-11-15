5eb8d6c15bdbf0ea2789d367c853aafa66bbd6a9
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-11-15 21:50:58 +0100

    Redsign view

--- toolkit/assets/testReport.js
+++ toolkit/assets/testReport.js
@@ -4,30 +4,15 @@
 function report(module, ok) {
 
     if (ok.every(elem => elem)) {
-        //document.writeln("All " + ok.length + " tests ok.");
-        //document.writeln(`<div class="block success">Test title - all ${ok.length} tests ok</div>`);
-        document.writeln(`<div class="block">
-                <div class="align">
-                  <div class="chip success">${ok.length} passed</div>
-                </div>
-                <div class="title">module: ${module}</div>
-                </div>`);
+        document.writeln(`
+            <div class="w3-panel w3-card">
+              <p> ${ok.length} passed - module: ${module}</p>
+            </div>`);
     } else {
         let failed = ok.filter(x => x === false);
-        document.writeln(`<div class="block">
-                            <div class="align">
-                              <div class="chip error">${failed.length} failed</div>
-                            </div>
-                            <div class="title">module: ${module}</div>
-                            </div>`);
+        document.writeln(`
+            <div class="w3-panel w3-card">
+              <p> ${failed.length} failed - module: ${module}</p>
+            </div>`);
     }
 }
-
-/*
-    <div class="block2">
-      <div class="chip">7 failed</div>
-      <div class="title">Module: Lambda Kalkuel</div>
-    </div>
-
-    <div class="block2"><div class="chip">7 failed</div><div class="title">Module: Title</div></div>
- */
