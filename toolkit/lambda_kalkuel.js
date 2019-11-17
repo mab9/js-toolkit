02c9108cb1955b2e709d4bd014fb4d0bb9b69834
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-11-17 12:02:49 +0100

    Fix wrong lambda kalkuel assertion

--- toolkit/lambda_kalkuel.js
+++ toolkit/lambda_kalkuel.js
@@ -1,22 +1,25 @@
 (() => {
 
     const testReportTitle = "Lambda Kalkuel";
-
-////////////////////////////////////////////////////////////////////////////
-// Lambda Kalkuel mit Tests
-// Alpha = Parameter umbenennen
-// Beta = Argumente einsetzen
-// Eta = Parameter kürzen
-
     let ok = [];
 
-// Alpha Translation
+    /////////////////////////////////////////////////////////////////////////   ///
+    // Lambda Kalkuel mit Tests
+    // - Alpha = Parameter umbenennen
+    // - Beta = Argumente einsetzen
+    // - Eta = Parameter kürzen
+
+
+
+    // Alpha Translation
     const id1 = x => x;   // function id3(x) {return x;}
     const id2 = y => y;
     ok.push(id1(5) === id2(5));
 
 
-// Beta Reduktion TODO add example
+
+
+    // Beta Reduktion TODO add example
     const red1 = f => x => f(x); // red(id1)(1);
     const red2 = x => id1(x);    // 1. Beta Reduktion
     const red3 = id1;            // 2. Beta Reduktion
@@ -30,10 +33,12 @@
     ok.push(red5 === 1);
 
 
-// Eta Reduktion
+
+
+    // Eta Reduktion
     const plus = x => y => x + y;
-//const foo = x => y => plus(x)(y); // 1. Eta reduktion
-//const foo = x =>      plus(x)   ; // 2. Eta reduktion
+    //const foo = x => y => plus(x)(y); // 1. Eta reduktion
+    //const foo = x =>      plus(x)   ; // 2. Eta reduktion
     const foo = plus; // 3. Eta reduktion
 
     ok.push(plus(2)(3) === 5);
@@ -41,16 +46,14 @@
     ok.push(plus(2)(3) === foo(2)(3));
 
 
-// Es folgen die atomaren Funktionen
+    // Es folgen die atomaren Funktionen
     const id = x => x;        // function id3(x) {return x;}
     const fst = x => y => x;  // gebe das erste Argument retour
 
     const snd = fst(id);      //
-    ok.push(fst(3)(4));
+    ok.push(fst(3)(4) === 3);
     ok.push(fst(id)(3)(4) === 4);  // gibt er den ersten Param aus oder den zweiten?
     ok.push(snd(3)(4) === 4);      // kleiner Trick -> damit er den zweiten ausgeben kann.
 
-
-    ok.push(false);
     report(testReportTitle, ok);
 })();
