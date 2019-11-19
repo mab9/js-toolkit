a231ba85bfdacac3996c2b70ced818f6fc2439cb
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-11-19 15:35:17 +0100

    stash changes

---
+++ toolkit/boolObject.js
@@ -0,0 +1,29 @@
+// requires /toolkit/utils/test.js
+
+test("Objekte als boolsche Operatoren", assert => {
+
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // Objekte als boolsche Operatoren
+
+
+    // 1. null
+    // 2. false
+    // 3. undefined
+    // 4. 0 (null)
+    // 5. "" empty string
+    // 6 NaN
+
+
+
+    // person
+
+    // let person
+
+    // let person = { }
+
+    // person && person.getAge && person.getAge()
+    // assert.equals(person, undefined);  --> console output
+
+
+});
---
+++ toolkit/toolkit.js
@@ -0,0 +1,46 @@
+const ToolkitController = () => {
+
+    const Tool = (name) => {
+        const showDetailsAttr = Observable(false);
+        return {
+            triggreShowDetails: showDetailsAttr.setValue(!showDetailsAttr.getValue()),
+            onTriggerShowDetails: showDetailsAttr.onChange
+        }
+    };
+
+    const toolsModel = ObservableList([]); // observable array of tools, this state is private
+
+
+    const addTool = (name) => {
+        const tool = Tool(name);
+        toolsModel.add(tool);
+        return tool;
+    };
+
+    return {
+        numberOfTools: toolsModel.count,
+        addTool: addTool,
+        onAddTool: toolsModel.onAdd
+        //onToolkitShow = () =>
+    }
+};
+
+const ToolItemView = (toolkitController, rootElement) => {
+
+    const render = tool => {
+        console.info("Render tool " + tool);
+        function createElements() {
+            const template = document.createElement('DIV'); // only for parsing
+            template.innerHTML = `
+                <div class="w3-panel w3-card">
+                  <p> module </p>
+                </div>           
+            `;
+            return template.children;
+        }
+        const [test] = createElements();
+        rootElement.appendChild(test());
+    };
+
+    toolkitController.onAddTool(render);
+};
