0c1c33e1c2bdeb67b4d6275a8776781391b4b2d3
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-12-16 19:05:39 +0100

    merge array descriptions into one js file

--- toolkit/arrays.js
+++ toolkit/arrays.js
@@ -1,16 +1,22 @@
 (() => {
 
-    const testReportTitle = "Arrays and useful tips";
-
-////////////////////////////////////////////////////////////////////////////
-//
-//  useful tips js tips - have a look at "mdn js XXX"
-// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array
-
+    const testReportTitle = "Arrays and some fancy array processing tricks";
     let ok = [];
 
-    let personen = ["bob", "alice", "foo"];
 
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // useful tips js tips - have a look at "mdn js XXX"
+    // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+
+    // Arrays functions
+    //
+    // - find
+    // - every
+    // - some
+
+    let personen = ["bob", "alice", "foo"];
 
     let found = personen.find(element => {
         console.log(element);                             // iterate over all, change elements
@@ -28,5 +34,46 @@
     ok.push(array1.some((it, index) => it === array2[index]));
     // gibt es iergend ein element, das in array 1 nicht in array 2 ist
 
+
+
+
+    // Arrays Comparison with additional functions
+    //
+    // - create helper functions for the array object
+    // - iterate over array content to compare content
+
+    Array.prototype.eq = function (second) {
+        const first = this;
+        if (first.length !== second.length) {
+            return false;
+        }
+        return !first.some((it, index) => second[index] !== it);
+    };
+
+
+    // Array Modeling with Splice and Slice
+    //
+    // - Remove, Shallow Copy, retrieve, exchange and some more tricks
+
+    const animals = ['ant', 'bison', 'animal', 'duck', 'elephant'];
+
+    ok.push(animals.slice(2).eq(["animal", "duck", "elephant"]));
+    ok.push(animals.slice(2, 4).eq(["animal", "duck"]));
+
+    let copy = animals.slice(0);  // shallow copy
+    ok.push(copy.eq(animals));
+
+    // splice statt slice
+    //copy = animals.splice(0,2); // ausversehen einen Schreibfehler...
+    //ok.push(!copy.eq(animals));
+
+    // Element aus einem array löschen
+    const animalDeleted = animals.splice(1, 1);
+    ok.push(['bison'].eq(animalDeleted));
+    ok.push(['ant', 'animal', 'duck', 'elephant'].eq(animals));
+
+    // Element aus einem Array austauschen, mit einem oder mehreren ...
+    ok.push([].eq(animals.splice(1, 0, "huu", "mab")));
+    ok.push(["ant", "huu", "mab", "animal", "duck", "elephant"].eq(animals));
     report(testReportTitle, ok);
 })();
